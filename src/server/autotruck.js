ee26625136cb2e628bef8f9ade705240cabc48c9
noberk <421471260@qq.com>
2018-05-20 14:43:44 +0800

    it is good to running

--- src/server/autotruck.js
+++ src/server/autotruck.js
@@ -3,6 +3,7 @@ exports.__esModule = true;
 var path = require("path");
 var devOption_1 = require("./devOption");
 var copy = require('recursive-copy');
+//需要被复制的文件夹
 var foldersName = ['data', 'resource'];
 var srcPath = path.resolve(__dirname, '..') + '\\'; //获取资源路径
 var buildPath = path.resolve(__dirname, '../../') + '\\' + devOption_1.dev.outputFolder;
